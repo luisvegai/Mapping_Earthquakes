2903ebdf9e8f9e773a4073c3a7681cab74c08b2b
luisvegai <lu1s.v@protonmail.ch>
2020-07-20 06:48:46 -0400

    Adding dashed line map

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -4,9 +4,9 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
-      40.7, -94.5
+      37.6213, -122.3790
     ],
-    zoom: 4
+    zoom: 5
   });
 
 //  Add a marker to the map for Los Angeles, California.
@@ -18,21 +18,37 @@ let map = L.map("mapid", {
 //  }).addTo(map);
 
 // Get data from cities.js
-let cityData = cities;
+// let cityData = cities;
 
 
 // Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-	console.log(city)
-  L.circleMarker(city.location, {
-    color: 'orange',
-    fillColor: '#FFA500',
-    weight: 4,
-    radius: city.population/100000
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+// cityData.forEach(function(city) {
+// 	console.log(city)
+//   L.circleMarker(city.location, {
+//     color: 'orange',
+//     fillColor: '#FFA500',
+//     weight: 4,
+//     radius: city.population/100000
+//   })
+//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+// });
+
+// Coordinates for each point to be used in the line.
+let line = [
+  [37.6213, -122.3790],
+  [30.1966119,-97.6694],
+  [43.6777215,-79.6270137],
+  [40.6423747,-73.7844661]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "blue",
+  weight: 4,
+  opacity: 0.5,
+  dashArray: '20,15',
+}).addTo(map);
 
   // We create the tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -68,7 +84,7 @@ cityData.forEach(function(city) {
 // mapbox://styles/mapbox/satellite-v9
 // mapbox://styles/mapbox/satellite-streets-v11
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         accessToken: API_KEY
